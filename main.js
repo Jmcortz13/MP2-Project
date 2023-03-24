b69ba9d75557b7bd5682ad686c39d8a97da5bfe6
Jessa Pelayo <jessamaepelayo21@gmail.com>
2023-03-24 17:38:30 +0800

    updated designs of index.html and markets.html

--- main.js
+++ main.js
@@ -24,27 +24,11 @@ function myFunction() {
       x.style.display = "block";
     }
   }
-
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-const menu_items = document.querySelectorAll('nav .mainMenu li a');
-
-
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-
-// close menu when you click on a menu item 
-menu_items.forEach(item => {
-    item.addEventListener('click',function(){
-        close();
-    })
-})
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-function close(){
-    mainMenu.style.top = '-100%';
-}
+  const menuIcon = document.querySelector('.menuIcon');
+  const mainMenu = document.querySelector('.mainMenu');
+  
+  menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('menuIcon--open');
+    mainMenu.classList.toggle('mainMenu--open');
+  });
+  
